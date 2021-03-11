e1e136f37cfac153edf0b9a4db1caa26192096dc
t.kaleton <t.kaleton@gmail.com>
2021-03-11 15:13:03 +0200

    Jeesibotin siirto herokuun

---
+++ jeesibot.py
@@ -0,0 +1,51 @@
+import discord
+import os
+import requests
+import random
+import json
+from gifit import gifit
+from myyntitekstit import myyntitekstit
+
+#from keep_alive import keep_alive
+
+
+client = discord.Client()
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+
+@client.event
+
+async def on_ready():
+  print("We have logged in as {0.user}".format(client))
+  print("Bot is now ready and operational")
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith("!dnd"):
+    await message.channel.send("**ÄÄNESTYS:** Mikä päivä pelataan? \r:one: = ma :two: = ti :three: = ke :four: = to :five: = pe :six: = la :seven: = su")
+
+  if message.content.startswith(('!scrim', '!scrims' , '!scrimit')):
+    await message.channel.send(gifit[random.randint(0,(len(gifit)-1))] )
+    await message.channel.send(myyntitekstit[random.randint(0,(len(myyntitekstit)-1))])
+
+  if message.content.startswith('!teevoileipa'):
+    await message.channel.send(":sandwich: ole hyvä!")
+
+  if message.content.startswith(('!rockmyday' , '!rockyourday' , '!ryd')):
+    quote = get_quote()
+    print("Sent following quote to server: " + quote +"\r\r" )
+    await message.channel.send(quote)
+    
+
+  if message.content.startswith(('!jeesi' , '!jeesibot')):
+    await message.channel.send("Jeesibotti tässä terve!\rOsaan seuraavat käskyt:\r!scrim / !scrims / !scrimit: Luo scrimikutsun tajunnanräjäyttävällä mainostekstillä :D \r !rockmyday / !rockyourday / !ryd: Tuo mietelauseen, joka mullistaa elämäsi\r !teevoileipa: Tekee voileivän")
+
+#keep_alive()
+client.run(os.getenv("TOKEN"))
+
---
+++ myyntitekstit.py
@@ -0,0 +1,16 @@
+myyntitekstit = [
+"\rPelipäivä bitses! :-DD \rHypätkääs odottamaan pelejä tuttuun ja turvalliseen tuksustaniaan! \rhttps://www.popflash.site/scrim/tuksustania",
+"\rSITÄ PELIÄ! \rhttps://www.popflash.site/scrim/tuksustania",
+"Let plei cee äs! wii hääv a jono at tuksustaniassa\rhttps://www.popflash.site/scrim/tuksustania",
+"Onko pelureita? :hamster:\rhttps://www.popflash.site/scrim/tuksustania",
+"Hyvän viban cs pelejä tyyliin nyt? Niksuttelu kielletty ofc \rhttps://www.popflash.site/scrim/tuksustania",
+"omae wa mou pelideiru \rhttps://www.popflash.site/scrim/tuksustania",
+"Pelipäivä on juhlapäivä - Timo Jutila\rhttps://www.popflash.site/scrim/tuksustania",
+"Laatuaikaa kavereiden kanssa, vai huutoa ja riitelyä runkkareita vastaan? click the link and find out!\rhttps://www.popflash.site/scrim/tuksustania",
+"Kuitenki haluat pelata\rhttps://www.popflash.site/scrim/tuksustania",
+"\rhttps://www.popflash.site/scrim/tuksustania","HIP HEI HUORAT, ON MEILLÄ KYRVÄT SUORAT! :\'D \rhttps://www.popflash.site/scrim/tuksustania",
+"UwU!! YAMETEEEEE\rhttps://www.popflash.site/scrim/tuksustania",
+"esl pro tour katowice hakee pelureita osoitteessa https://www.popflash.site/scrim/tuksustania",
+"do juu want tu leikki? https://www.popflash.site/scrim/tuksustania",
+"Olisiko se pelihommels? https://www.popflash.site/scrim/tuksustania"
+]
