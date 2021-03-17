19a04ab7169639261fd1e485420c7f9430dca7ac
t.kaleton <t.kaleton@gmail.com>
2021-03-17 19:15:56 +0200

    korjattiin bugi, jossa \!scrimiasdasd tuotti scrimikutsun

--- jeesibot.py
+++ jeesibot.py
@@ -2,7 +2,9 @@ import discord
 import os
 import requests
 import random
+import time
 import json
+import discord.ext
 from gifit import gifit
 from myyntitekstit import myyntitekstit
 from musattimet import musat
@@ -25,30 +27,31 @@ async def on_ready():
   print("Bot is now ready and operational")
 @client.event
 async def on_message(message):
-  if message.author == client.user:
+  if message.author == client.user: 
     return
-
+    
   if message.content.startswith("!dnd"):
     await message.channel.send("**ÄÄNESTYS:** Mikä päivä pelataan? \r:one: = ma :two: = ti :three: = ke :four: = to :five: = pe :six: = la :seven: = su")
 
-  if message.content.startswith(('!scrim', '!scrims' , '!scrimit')):
+  if message.content.startswith(('!scrim ', '!scrims ' , '!scrimit ')):
     await message.channel.send(gifit[random.randint(0,(len(gifit)-1))] )
     await message.channel.send(myyntitekstit[random.randint(0,(len(myyntitekstit)-1))])
 
-  if message.content.startswith('!teevoileipa'):
+  if message.content.startswith('!teevoileipa '):
     await message.channel.send(":sandwich: ole hyvä!")
 
-  if message.content.startswith(('!rockmyday' , '!rockyourday' , '!ryd')):
+  if message.content.startswith(('!rockmyday ' , '!rockyourday ' , '!ryd ')):
     quote = get_quote()
     print("Sent following quote to server: " + quote +"\r\r" )
     await message.channel.send(quote)
     
-  if message.content.startswith('!taustamelua'):
+  if message.content.startswith('!taustamelua '):
     random.shuffle(musat)
+
     for biisi in musat:
       await message.channel.send("!play " + biisi)
-
-  if message.content.startswith(('!jeesi' , '!jeesibot')):
+  
+  if message.content.startswith(('!jeesi ' , '!jeesibot ')):
     await message.channel.send("Jeesibotti tässä terve!\rOsaan seuraavat käskyt:\r!scrim / !scrims / !scrimit: Luo scrimikutsun tajunnanräjäyttävällä mainostekstillä :D \r !rockmyday / !rockyourday / !ryd: Tuo mietelauseen, joka mullistaa elämäsi\r !teevoileipa: Tekee voileivän")
 
 #keep_alive()
